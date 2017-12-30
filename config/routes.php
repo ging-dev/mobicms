bc6292324638adb43eeff04222424da4abd49480
Oleg Kasyanov <dev@mobicms.net>
2017-12-30 12:42:33 +0400

    Changed route

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,6 @@
 
 /** @var $app Zend\Expressive\Application */
 
-$app->get('/', Johncms\App\Homepage\HomepageAction::class, 'home');
+$app->get('/', Johncms\Modules\Homepage\HomepageAction::class, 'home');
 
 //$app->get('/news[/]', Johncms\App\NewsAction::class, 'news');
