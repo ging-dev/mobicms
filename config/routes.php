acb875ed8d3f73ceac46820190fe743cbc557b8d
Oleg Kasyanov <dev@mobicms.net>
2018-01-20 23:33:18 +0400

    Updated routes configuration

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,9 @@
 
 /** @var $app Zend\Expressive\Application */
 
-$app->get('/', Johncms\Modules\Homepage\HomepageAction::class, 'home');
+$app->get('/', [
+    Johncms\Checkpoint\AuthenticationMiddleware::class,
+    Johncms\Modules\HomePage\HomepageAction::class,
+], 'home');
 
 //$app->get('/news[/]', Johncms\App\NewsAction::class, 'news');
